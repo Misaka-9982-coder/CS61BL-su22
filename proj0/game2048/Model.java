58b4c6ed2938cffc13cf98173a3e1b878bfbef9c
misaka <1784321894@qq.com>
2022-07-02 16:58:58 +0800

    lab0: Completed Model.java emptySpaceExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -135,6 +135,14 @@ public class Model extends Observable {
      */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        int size = b.size();
+        for(int i = 0; i < size; i ++ ) {
+            for(int j = 0; j < size; j ++ ) {
+                if(b.tile(i, j) == null) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
