e9348421a58953882c9342fe7c4ae6d8b0ef9205
misaka <1784321894@qq.com>
2022-07-02 17:53:53 +0800

    lab0: Completed Model.java atLeastOneMoveExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -171,6 +171,17 @@ public class Model extends Observable {
      */
     public static boolean atLeastOneMoveExists(Board b) {
         // TODO: Fill in this function.
+        if(emptySpaceExists(b)) return true;
+        for(int i = 0; i < b.size(); i ++ ) {
+            for(int j = 0; j < b.size(); j ++ ) {
+                if(i + 1 < b.size() && b.tile(i, j).value() == b.tile(i + 1, j).value()) {
+                    return true;
+                }
+                if(j + 1 < b.size() && b.tile(i, j).value() == b.tile(i, j + 1).value()) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
