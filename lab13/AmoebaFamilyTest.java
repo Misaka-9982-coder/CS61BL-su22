fac9e5c2f64d5621ce5c14ab07ff190137fa651e
misaka <1784321894@qq.com>
2022-07-16 16:18:43 +0800

    lab13: testLongestName

---
+++ lab13/AmoebaFamilyTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+public class AmoebaFamilyTest {
+
+    @Test
+    public void testLongestName() {
+        AmoebaFamily family = new AmoebaFamily("Amos McCoy");
+        family.addChild("Amos McCoy", "mom/dad");
+        family.addChild("Amos McCoy", "auntie");
+        family.addChild("mom/dad", "me");
+        family.addChild("mom/dad", "Fred");
+        family.addChild("mom/dad", "Wilma");
+        family.addChild("me", "Mike");
+        family.addChild("me", "Homer");
+        family.addChild("me", "Marge");
+        family.addChild("Mike", "Bart");
+        family.addChild("Mike", "Lisa");
+        family.addChild("Marge", "Bill");
+        family.addChild("Marge", "Hilary");
+        System.out.println("Here's the family!");
+        System.out.println(family.longestName());
+        System.out.println(family.longestNameLength());
+    }
+}
