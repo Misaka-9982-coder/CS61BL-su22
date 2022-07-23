2c32fcb0b3051570c0e739ffbc5a5355dafa52e7
misaka <1784321894@qq.com>
2022-07-23 16:24:22 +0800

    lab18 : Constructor

--- lab18/HashMap.java
+++ lab18/HashMap.java
@@ -1,10 +1,82 @@
-public class HashMap {
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashMap <K, V> implements Map61BL<K, V> {
 
     /* TODO: Instance variables here */
 
+    private int size;
+    private int capacity;
+
+    private double loadFactor;
+
+    private HashSet<K> keys;
+
     /* TODO: Constructors here */
 
+    public HashMap() {
+        this.size = 0;
+        this.capacity = 16;
+        this.loadFactor = 0.75;
+    }
+
+    public HashMap(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        this.loadFactor = 1;
+    }
+
+    public HashMap(int capacity, double loadFactor) {
+        this.size = 0;
+        this.capacity = capacity;
+        this.loadFactor = loadFactor;
+    }
+
+    public int capacity() {
+        return capacity;
+    }
+
     /* TODO: Interface methods here */
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(K key, V value) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
 
     private static class Entry {
 
