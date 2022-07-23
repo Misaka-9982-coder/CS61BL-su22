931843689cb174059eb0285c69039cc71e1bdf50
misaka <1784321894@qq.com>
2022-07-23 17:04:45 +0800

    lab18 : contains, puts, get, resize

--- lab18/HashMap.java
+++ lab18/HashMap.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -12,28 +13,41 @@ public class HashMap <K, V> implements Map61BL<K, V> {
 
     private HashSet<K> keys;
 
+    Entry[] HashTable;
+
     /* TODO: Constructors here */
 
     public HashMap() {
         this.size = 0;
         this.capacity = 16;
         this.loadFactor = 0.75;
+        HashTable = new Entry[capacity];
     }
 
     public HashMap(int capacity) {
         this.size = 0;
         this.capacity = capacity;
         this.loadFactor = 1;
+        HashTable = new Entry[capacity];
     }
 
     public HashMap(int capacity, double loadFactor) {
         this.size = 0;
         this.capacity = capacity;
         this.loadFactor = loadFactor;
+        HashTable = new Entry[capacity];
     }
 
     public int capacity() {
         return capacity;
+    }
+
+    public int hashCode(K key) {
+        int index =  (int) (Math.abs(key.hashCode()) % capacity);
+        while(HashTable[index] != null && HashTable[index].key != key) {
+            index = (index + 1) % capacity;
+        }
+        return index;
     }
 
     /* TODO: Interface methods here */
@@ -45,17 +59,39 @@ public class HashMap <K, V> implements Map61BL<K, V> {
 
     @Override
     public boolean containsKey(K key) {
-        return false;
+        int index = hashCode(key);
+        if(HashTable[index] == null) {
+            return false;
+        }
+        return HashTable[index].key == key;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = hashCode(key);
+        return (V) HashTable[index].value;
     }
 
     @Override
     public void put(K key, V value) {
+        if (size >= capacity * loadFactor) {
+            resize();
+        }
+        int index = hashCode(key);
+        Entry entry = new Entry(key, value);
+        HashTable[index] = entry;
+        this.size ++ ;
+    }
 
+    public void resize() {
+        capacity = capacity * 2;
+        Entry[] tmp = new Entry[capacity];
+        for (int i = 0; i < size; i++) {
+            if (HashTable[i] != null) {
+                tmp[i] = HashTable[i];
+            }
+        }
+        HashTable = tmp;
     }
 
     @Override
@@ -78,12 +114,12 @@ public class HashMap <K, V> implements Map61BL<K, V> {
         return null;
     }
 
-    private static class Entry {
+    private static class Entry<K, V> {
 
-        private String key;
-        private String value;
+        private K key;
+        private V value;
 
-        Entry(String key, String value) {
+        Entry(K key, V value) {
             this.key = key;
             this.value = value;
         }
